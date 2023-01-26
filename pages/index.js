a1ce74c0131d274cfd507fa0a4606ccd8339a81a
Jackson <jackson@jacksonmoody.com>
2023-01-26 17:20:06 -0500

    Added favicon

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Home from '../components/home'
 import Projects from '../components/projects'
+import Head from 'next/head'
 
 export default function Main() {
   return (
-    <div className="main">
-      <Home />
-      <Projects />
-    </div>
+    <>
+      <Head>
+        <title>PS70 Portfolio: Home</title>
+      </Head>
+      <div className="main">
+        <Home />
+        <Projects />
+      </div>
+    </>
+
   )
 }
