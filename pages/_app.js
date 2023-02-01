11b8d3017f7b69897d853b594b949e5499e60248
Jackson <jackson@jacksonmoody.com>
2023-02-01 00:28:46 -0500

    Fixed styling

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
         <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico?" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
--- pages/about.js
+++ pages/about.js
@@ -16,25 +16,25 @@ export default function About() {
       <Header image="https://i.ibb.co/QcnymSY/Screenshot-2023-01-27-at-1-30-42-AM.jpg" title="About Me" />
 
       <div className={styles.content}>
-        <Grid container alignItems="flex-start" justifyContent="center" paddingLeft={15} paddingRight={15} paddingTop={10}>
+        <Grid container alignItems="flex-start" justifyContent="center" columnSpacing={{ xs: 15}} sx={{marginTop: 5}}>
           <Grid item xs={12} order={{ xs: 1, sm: 1 }}>
             <div className={styles.section}>
               Welcome!
             </div>
           </Grid>
-          <Grid item xs={12} sm={8} order={{ xs: 3, sm: 2 }}>
+          <Grid item xs={12} sm={9} order={{ xs: 3, sm: 2 }}>
             <div className={styles.text}>
               <p>My name is Jackson Moody, and I'm a freshman at Harvard College studying Mechanical Engineering and Computer Science.</p>
               <p>While I was born just a few minutes from Harvard, I have lived most of my life in Boulder, Colorado. When I'm not grinding over psets, you can probably find me skiing, biking, or <span>&#40;</span>badly<span>&#41;</span> playing the piano. </p>
               <p>At Harvard, I am also a member of <a href="https://socialgood.hcs.harvard.edu/" target="_blank" className={styles.link}>Tech for Social Good</a>, <a href="https://hces.seas.harvard.edu/" target="_blank" className={styles.link}>Harvard College Engineering Society</a>, <a href="https://www.harvardaadt.org/" target="_blank" className={styles.link}>The Asian American Dance Troupe</a>, various theater productions, and a bunch of other random clubs lol.</p>
-              <p>Of course, I love to make things—ranging from 3D printed trinkets to laser cut ornaments. I can't wait to be in PS70 this semester!</p>
+              <p>Of course, I also love to make things—ranging from 3D printed trinkets to laser cut ornaments. I can't wait to be in PS70 this semester!</p>
             </div>
 
             <Link href="/">
               <button className={styles.button}>Return Home</button>
             </Link>
           </Grid>
-          <Grid item xs={12} sm={4} order={{ xs: 2, sm: 3 }}>
+          <Grid item xs={12} sm={3} order={{ xs: 2, sm: 3 }}>
             <div className={styles.circle}>
               <Image src={headshot} alt="Headshot" width={300} />
             </div>
